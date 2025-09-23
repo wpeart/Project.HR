07fb18a4d2be3445a3484e61f845577689e0ce5e
Wilbert Peart <wilbert.peart@techcocreators.com>
2025-09-22 19:05:49 -0600

    Add HttpClient and EmployeeHelper for API integration
    Added an `HttpClient` service in `Program.cs` named "AdminApi"
    with its base address configured via the new `ApiUrl` key in
    `appsettings.json`. Introduced the `EmployeeHelper` class to
    demonstrate API interaction using `IHttpClientFactory`.
    Included a placeholder method `GetEmployeesAsync` (commented out)
    to fetch employee data from an API.
    HR-3

---
+++ Project.HR.UI.Blazor/Helpers/EmployeeHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Project.HR.UI.Blazor.Helpers
+{
+    public class EmployeeHelper
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public EmployeeHelper(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+       /* public async Task<List<EmployeeDTO>> GetEmployeesAsync()
+        {
+            var client = _clientFactory.CreateClient("HRApiClient");
+            var response = await client.GetAsync("api/employees");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<Employee>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+            return new List<Employee>();
+        }*/
+
+    }
+}
--- Project.HR.UI.Blazor/Program.cs
+++ Project.HR.UI.Blazor/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient("AdminApi", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiUrl"]);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
