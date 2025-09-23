c22fd99654164ed05a46a5dd33fa808ed94d80a1
Wilbert Peart <wilbert.peart@techcocreators.com>
2025-09-22 19:14:12 -0600

    Start the development of the EmployeeHelper HR-3

--- Project.HR.UI.Blazor/Helpers/EmployeeHelper.cs
+++ Project.HR.UI.Blazor/Helpers/EmployeeHelper.cs
@@ -1,4 +1,7 @@
-﻿namespace Project.HR.UI.Blazor.Helpers
+﻿using Project.HR.Domain.DTOs;
+using System.Text.Json;
+
+namespace Project.HR.UI.Blazor.Helpers
 {
     public class EmployeeHelper
     {
@@ -9,17 +12,17 @@
             _clientFactory = clientFactory;
         }
 
-       /* public async Task<List<EmployeeDTO>> GetEmployeesAsync()
+        public async Task<List<EmployeeDTO>> GetEmployeesAsync()
         {
             var client = _clientFactory.CreateClient("HRApiClient");
             var response = await client.GetAsync("api/employees");
             if (response.IsSuccessStatusCode)
             {
                 var jsonString = await response.Content.ReadAsStringAsync();
-                return JsonSerializer.Deserialize<List<Employee>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return JsonSerializer.Deserialize<List<EmployeeDTO>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
-            return new List<Employee>();
-        }*/
+            return new List<EmployeeDTO>();
+        }
 
     }
 }
